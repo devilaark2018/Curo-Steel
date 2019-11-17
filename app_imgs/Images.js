487ff51128e1f8adc446a76b8e0deeeeb97fc21b
Natsu Dragoneel <pranay.aark@gmail.com>
2019-11-18 03:03:12 +0530

    Added images

--- app_imgs/Images.js
+++ app_imgs/Images.js
@@ -1,6 +1,9 @@
 const app_logo_Img = require('../assets/imgs/kiw_logo_lg.png');
+const app_logo_Img_alt = require('../assets/imgs/kiw_logo_sm.png');
+
 const BackgroundImg = require("../assets/imgs/bg_reactnative.png");
 export default{
     app_logo_Img,
-    BackgroundImg
+    BackgroundImg,
+    app_logo_Img_alt
 }
